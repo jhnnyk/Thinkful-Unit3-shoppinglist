5abeaa49f71753ead02f3376d306c94ba92a2df0
John Knotts <jhnnyk@gmail.com>
2017-08-20 15:51:40 -0600

    add an item to the list

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// your code here! 
-// and remember you'll need to link to this file
-// in a <script> element in index.html
+function shoppingListHandler() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault()
+    const shoppingItem = $('.js-shopping-list-entry').val()
+    $('.shopping-list').prepend(`
+    <li>
+      <span class="shopping-item">${shoppingItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+    `)
+  })
+}
+
+shoppingListHandler();
